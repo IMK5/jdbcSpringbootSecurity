eee22abbb4eb9fd94a5b174b9cf29e88f84b7642
IMK5 <61726156+IMK5@users.noreply.github.com>
2020-04-15 17:46:50 +0300

    buils project

---
+++ src/main/java/imk/tn/jdbcSpringbootSecurity/JdbcSpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package imk.tn.jdbcSpringbootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSpringbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imk/tn/jdbcSpringbootSecurity/JdbcSpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package imk.tn.jdbcSpringbootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
